2cac0f7bf359fe0ebcb5cf8f81d8633f25ffee6b
sefinehtesfa <sefinehtesfa@gmail.com>
2024-05-02 21:59:36 +0300

    Populate and schema

--- flask_app/populate.py
+++ flask_app/populate.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-
+from datetime import datetime, timedelta
 def populate_profile(profile, user_data):
     # Helper function to populate a profile with data from a dictionary
     profile.username = user_data.get('username')
@@ -24,3 +22,38 @@ def populate_profile(profile, user_data):
     profile.is_followed = False
     return profile
 
+def convert_milliseconds_to_datetime(ms):
+    """Convert milliseconds since epoch to a datetime object."""
+    return datetime(1970, 1, 1) + timedelta(milliseconds=ms)
+
+def populate_casts(cast, cast_data):
+    cast.casts_hash = cast_data.get('hash')
+    cast.thread_hash = cast_data.get('threadHash')
+    cast.parent_source_type = cast_data.get('parentSource', {}).get('type')
+    cast.parent_source_url = cast_data.get('parentSource', {}).get('url')
+    cast.author_fid = cast_data.get('author', {}).get('fid')
+    cast.author_username = cast_data.get('author', {}).get('username')
+    cast.author_display_name = cast_data.get('author', {}).get('displayName')
+    cast.author_pfp_url = cast_data.get('author', {}).get('pfp', {}).get('url')
+    cast.author_pfp_verified = cast_data.get('author', {}).get('pfp', {}).get('verified', False)
+    cast.author_profile_bio = cast_data.get('author', {}).get('profile', {}).get('bio', {}).get('text', '')
+    cast.author_profile_location_description = cast_data.get('author', {}).get('profile', {}).get('location', {}).get('description', '')
+    cast.author_follower_count = cast_data.get('author', {}).get('followerCount', 0)
+    cast.author_following_count = cast_data.get('author', {}).get('followingCount', 0)
+    cast.author_active_on_fc_network = cast_data.get('author', {}).get('activeOnFcNetwork', False)
+    cast.message_text = cast_data.get('text', '')
+    
+    # Convert timestamp
+    timestamp_ms = cast_data.get('timestamp')
+    if timestamp_ms is not None:
+        cast.timestamp = convert_milliseconds_to_datetime(timestamp_ms)
+    else:
+        cast.timestamp = datetime.now()  # or use default, if appropriate
+
+    cast.replies_count = cast_data.get('replies', {}).get('count', 0)
+    cast.reactions_count = cast_data.get('reactions', {}).get('count', 0)
+    cast.recasts_count = cast_data.get('recasts', {}).get('count', 0)
+    cast.watches_count = cast_data.get('watches', {}).get('count', 0)
+    cast.quote_count = cast_data.get('quoteCount', 0)
+    cast.combined_recast_count = cast_data.get('combinedRecastCount', 0)
+    
